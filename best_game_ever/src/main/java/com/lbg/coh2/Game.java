33534235af5a316753b13e66c76f06fb33588e8b
ali <alirezanoori02121@gmail.com>
2022-10-21 14:14:56 +0100

    Game class modified

--- best_game_ever/src/main/java/com/lbg/coh2/Game.java
+++ best_game_ever/src/main/java/com/lbg/coh2/Game.java
@@ -1,5 +1,27 @@
 package com.lbg.coh2;
 
 public class Game {
+	
+      private int goalX, goalY,playerX, playerY;
+	  
+	  
+	  public void Game() {
+		  
+		  goalX=0;
+		  goalY=7;
+		  playerX=0;
+		  playerY=3;
+		  
+	  }
+	  
+	  public double getDistance() {
+			
+			return (Math.sqrt(Math.pow(playerX-goalX,2) + Math.pow(playerY- goalY,2)));   
+		  }
+		  
+	  }
+	  
+	  
+	  
 
-}
+
