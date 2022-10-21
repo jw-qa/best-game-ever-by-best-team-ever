ac15335e807bfb39e0c403edd20165f7438f599a
ali <alirezanoori02121@gmail.com>
2022-10-21 14:32:44 +0100

    Game class modified2

--- best_game_ever/src/main/java/com/lbg/coh2/Game.java
+++ best_game_ever/src/main/java/com/lbg/coh2/Game.java
@@ -2,8 +2,7 @@ package com.lbg.coh2;
 
 public class Game {
 	
-      private int goalX, goalY,playerX, playerY;
-	  
+      private int goalX, goalY,playerX, playerY,boardW,boardH;
 	  
 	  public void Game() {
 		  
@@ -11,13 +10,36 @@ public class Game {
 		  goalY=7;
 		  playerX=0;
 		  playerY=3;
-		  
+		  boardW=10;
+		  boardH=10;
 	  }
 	  
 	  public double getDistance() {
 			
 			return (Math.sqrt(Math.pow(playerX-goalX,2) + Math.pow(playerY- goalY,2)));   
 		  }
+	  
+	  public void updatePosition(String move) {
+		  
+
+		  if ((move.equals("Left") || move.equals("West")) && playerX != 0)
+			  playerX -- ;
+		  //TODO use getters and setters
+		  else if((move.equals("Right") || move.equals("East")) && playerX !=boardW)
+			  playerX ++ ;
+		  else if((move.equals("Down") || move.equals("South")) && playerY !=0)
+			  playerY -- ;
+		  else if((move.equals("Up") || move.equals("North")) && playerY !=boardH)
+			  playerY ++ ;
+		  else
+			  System.out.print("You are at the end of the world");
+			
+		  }
+	  public void checkIfReachGoal()
+	  {
+		  if (goalX == playerX && playerY==goalY)
+			  System.out.print("You Win");
+	  }
 		  
 	  }
 	  
